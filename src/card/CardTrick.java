90f2842bc0ccded9fe60ff8a37e576bbb9c2928a
Mboghdady-94 <elboghda@sheridancollege.ca>
2026-01-19 16:54:30 -0500

    student project v1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,6 +4,8 @@
  */
 package card;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +22,39 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random() * 13) + 1);
+            
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])            card.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
-    }
+        Scanner input = new Scanner(System.in);
+   
+        System.out.println("Pick a card - any card!");
+        System.out.print("Enter card value (1-13): ");
+        int userValue = input.nextInt();
+        System.out.print("Enter suit (Hearts, Diamonds, Clubs, Spades): ");
+        String userSuit = input.next();
+
+        // Search for the card in magicHand
+        boolean found = false;
+        for (int i = 0; i < magicHand.length; i++) {
+            if (magicHand[i].getValue() == userValue && 
+                magicHand[i].getSuit().equalsIgnoreCase(userSuit)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Congratulations! Your card is in the magic hand!");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+         }
     
 }
